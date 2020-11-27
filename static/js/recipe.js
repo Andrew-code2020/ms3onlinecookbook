647c9814563911acb47fe3d3c7fcde5c94fa3af3
Andrew Burns <burnsad@tcd.ie>
2020-11-27 13:50:36 +0000

    added navbar style and resolved materialize js stuff

--- static/js/recipe.js
+++ static/js/recipe.js
@@ -1,42 +1,19 @@
-$(document).ready(function () {
-    $(".activity2").hide();
-    $(".culture2").hide();
-    $(".wellness2").hide();
-    $(".cities2").hide();    
-});
+$(document).ready(function(){
+     $(".sidenav").sidenav({edge: "right"});
+   });
+
+/*--document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+  });--*/
+
+  console.log('linked properly');
 
 /*---top picture---*/
-$(".activity" )
-  .mouseenter(function() {
-    $(".activity2").show();
-  })
-  .mouseleave(function() {
-    $(".activity2").hide();
-  });
 
-  /*---middle picture 1---*/
-$(".culture" )
-  .mouseenter(function() {
-    $(".culture2").show();
-  })
-  .mouseleave(function() {
-    $(".culture2").hide();
-  });
+/*---middle picture 1---*/
 
-  /*---middle picture 2---*/
-  $(".wellness" )
-  .mouseenter(function() {
-    $(".wellness2").show();
-  })
-  .mouseleave(function() {
-    $(".wellness2").hide();
-  });
-
-  /*---bottom picture 1---*/
-  $(".cities")
-  .mouseenter(function() {
-    $(".cities2").show();
-  })
-  .mouseleave(function() {
-    $(".cities2").hide();
-  });
+/*---middle picture 2---*/
+  
+/*---bottom picture 1---*/
+  
